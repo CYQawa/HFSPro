8587b8a489468a500c0ad6beca05a2133789f2c1
CYQawa <2128142988@qq.com>
2026-02-08 16:01:37 +0800

    主页布局框架完成

---
+++ app/src/main/java/com/cyq/awa/hfspro/activities/MainActivity.java
@@ -0,0 +1,69 @@
+package com.cyq.awa.hfspro.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.cyq.awa.hfspro.R;
+import com.cyq.awa.hfspro.fragments.HomeFragment;
+import com.cyq.awa.hfspro.fragments.MineFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+  private FragmentContainerView fragmentContainerView;
+  private HomeFragment homefragment;
+  private MineFragment mineFragment;
+  private FragmentManager fragmentManager;
+  private Fragment nowFragment;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    fragmentContainerView = findViewById(R.id.fragment_container);
+    BottomNavigationView bottomNavigationView = findViewById(R.id.main_nav_view);
+
+    homefragment = new HomeFragment();
+    mineFragment = new MineFragment();
+
+    fragmentManager = getSupportFragmentManager();
+    fragmentManager
+        .beginTransaction()
+        .add(R.id.fragment_container, homefragment)
+        .add(R.id.fragment_container, mineFragment)
+        .hide(mineFragment)
+        .commit();
+
+    showFragment(homefragment);
+
+    bottomNavigationView.setOnItemSelectedListener(
+        item -> {
+          int id = item.getItemId();
+          if (id == R.id.nav_home) {
+            showFragment(homefragment);
+            return true;
+          } else if (id == R.id.nav_profilye) {
+            showFragment(mineFragment);
+            return true;
+          }
+          return true;
+        });
+  }
+
+  private void showFragment(Fragment aimfragment) {
+    if (aimfragment == nowFragment) return;
+    FragmentTransaction fragmentt = fragmentManager.beginTransaction();
+    fragmentt.setCustomAnimations(
+        R.anim.mtrl_bottom_sheet_slide_in, R.anim.mtrl_bottom_sheet_slide_out);
+    if (nowFragment != null) {
+      fragmentt.hide(nowFragment);
+    }
+    fragmentt.show(aimfragment);
+    fragmentt.commit();
+
+    nowFragment = aimfragment;
+  }
+}
