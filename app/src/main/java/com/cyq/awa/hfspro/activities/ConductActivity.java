d6f32f777364e21934190744a77be79e11615cbf
CYQawa <2128142988@qq.com>
2026-02-08 17:13:09 +0800

    主页标题

---
+++ app/src/main/java/com/cyq/awa/hfspro/activities/ConductActivity.java
@@ -0,0 +1,13 @@
+package com.cyq.awa.hfspro.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.cyq.awa.hfspro.R;
+
+public class ConductActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conduct);
+    }
+}
--- app/src/main/java/com/cyq/awa/hfspro/activities/MainActivity.java
+++ app/src/main/java/com/cyq/awa/hfspro/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cyq.awa.hfspro.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -22,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
+    
+    startActivity(new Intent(MainActivity.this, ConductActivity.class));
+    
     fragmentContainerView = findViewById(R.id.fragment_container);
     BottomNavigationView bottomNavigationView = findViewById(R.id.main_nav_view);
 
