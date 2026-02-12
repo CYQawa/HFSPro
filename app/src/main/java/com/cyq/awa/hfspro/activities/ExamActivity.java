fc6b89b1a2af65da7db3e6d8e5f0644429abc756
CYQawa <2128142988@qq.com>
2026-02-12 22:10:26 +0800

    Exam页布局

--- app/src/main/java/com/cyq/awa/hfspro/activities/ExamActivity.java
+++ app/src/main/java/com/cyq/awa/hfspro/activities/ExamActivity.java
@@ -1,25 +1,53 @@
 package com.cyq.awa.hfspro.activities;
 
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.TypedValue;
 import androidx.appcompat.app.AppCompatActivity;
 import com.cyq.awa.hfspro.R;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.cyq.awa.hfspro.tools.MyModel.MyExam;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.android.material.textview.MaterialTextView;
+
 public class ExamActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exam);
-        
-        MyExam exam = (MyExam) getIntent().getSerializableExtra("myexam");
-        
-        MaterialToolbar toolbar = findViewById(R.id.toolbar);
-        CollapsingToolbarLayout tooltitle = findViewById(R.id.tooltitle);
-        
-        toolbar.setNavigationOnClickListener(v ->{
-            finish();
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_exam);
+
+    MyExam exam = (MyExam) getIntent().getSerializableExtra("myexam");
+    MaterialToolbar toolbar = findViewById(R.id.toolbar);
+    CollapsingToolbarLayout tooltitle = findViewById(R.id.tooltitle);
+    CircularProgressIndicator progressIndicator = findViewById(R.id.btn_ProgressIndicator);
+    MaterialTextView scoretext = findViewById(R.id.scoretext);
+    MaterialTextView manfent = findViewById(R.id.manfen);
+
+    scoretext.setText(exam.getScore());
+    manfent.setText("/" + exam.getManfen());
+
+    toolbar.setNavigationOnClickListener(
+        v -> {
+          finish();
         });
-        tooltitle.setTitle(exam.getName());
+    tooltitle.setTitle(exam.getName());
+    progressIndicator.setIndeterminate(false);
+    try {
+      double score = Double.parseDouble(exam.getScore());
+      int manfen = exam.getManfen();
+
+      if (manfen > 0) {
+        int progress = (int) Math.round((score / manfen) * 100);
+        progressIndicator.setProgress(progress);
+      } else {
+        progressIndicator.setProgress(0);
+      }
+    } catch (NumberFormatException e) {
+      e.printStackTrace();
+      // 分数格式非法时，进度归零
+      progressIndicator.setProgress(0);
     }
+  }
 }
