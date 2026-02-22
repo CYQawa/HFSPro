b43d21d66cf78428e82acce9b56bdca19471eb1f
CYQawa <2128142988@qq.com>
2026-02-22 12:29:24 +0800

    修复了图片模糊问题

--- app/src/main/java/com/cyq/awa/hfspro/activities/AnswerActivity.java
+++ app/src/main/java/com/cyq/awa/hfspro/activities/AnswerActivity.java
@@ -45,10 +45,7 @@ public class AnswerActivity extends AppCompatActivity {
         });
     tooltitle.setTitle(paper.getSubject() + "：原卷/答题卡");
 
-    // 测试用，伪数据
-    List<String> i = new ArrayList<String>();
-    i.add(
-        "https://yj-oss.yunxiao.com/v1/baidu-raw/origin/10062333/69872a9f46e19da95fe1e1b4.png?authorization=bce-auth-v1%2Fa908715249bb41c998c7d924b2476b37%2F2026-02-21T10%3A33%3A06Z%2F604800%2Fhost%2F06bbf9dacaa5302574df7ba932bc79abaf5bcf7a866bbb49a228faf8a792c782");
+    
 
     //    ViewPagerAdapter adapter = new ViewPagerAdapter(this, i);
     //    viewPager2.setAdapter(adapter);
--- app/src/main/java/com/cyq/awa/hfspro/activities/ImagePreviewActivity.java
+++ app/src/main/java/com/cyq/awa/hfspro/activities/ImagePreviewActivity.java
@@ -1,11 +1,12 @@
 package com.cyq.awa.hfspro.activities;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;          // 如果使用Glide加载网络图片
+import com.bumptech.glide.request.target.Target;
 import com.cyq.awa.hfspro.R;
 import com.github.chrisbanes.photoview.PhotoView;
+import com.google.android.material.snackbar.Snackbar;
 public class ImagePreviewActivity extends AppCompatActivity {
 
     @Override
@@ -14,6 +15,7 @@ public class ImagePreviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image_preview);
 
         PhotoView photoView = findViewById(R.id.photo_view);
+        
 
         // 获取传递的图片资源ID
         int imageRes = getIntent().getIntExtra("image_res", 0);
@@ -23,8 +25,9 @@ public class ImagePreviewActivity extends AppCompatActivity {
         } else {
             // 如果是网络图片，可以用Glide加载
             String imageUrl = getIntent().getStringExtra("image_url");
-            Glide.with(this).load(imageUrl).into(photoView);
+            Glide.with(this).load(imageUrl).override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL).into(photoView);
         }
+        Snackbar.make(findViewById(R.id.background),"按返回键返回",Snackbar.LENGTH_SHORT).show();
 
         // PhotoView 自带缩放、平移功能，无需额外代码
         // 可选：设置最大缩放比例
